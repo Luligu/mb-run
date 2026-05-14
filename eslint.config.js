9f87d62503e63a4ab80c2003ca163b41547439ef
Luligu <target.epithet-0h@icloud.com>
2026-05-14 15:37:34 +0100

    Package update

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-// eslint.config.js 2.0.2
+// eslint.config.js 2.0.4
 
 // This ESLint configuration is designed for a TypeScript project using ESM modules.
 
@@ -46,7 +46,6 @@ export default defineConfig([
     name: 'JavaScript & TypeScript Source Files',
     files: sourceFiles,
     plugins: {
-      js,
       n,
       jsdoc,
       'simple-import-sort': importsort,
@@ -70,6 +69,7 @@ export default defineConfig([
           vars: 'all',
           args: 'after-used',
           ignoreRestSiblings: true,
+          reportUsedIgnorePattern: true, // Error when a _ prefixed variable is actually used
           varsIgnorePattern: '^_', // Ignore unused variables starting with _
           argsIgnorePattern: '^_', // Ignore unused arguments starting with _
           caughtErrorsIgnorePattern: '^_', // Ignore unused caught errors starting with _
@@ -171,7 +171,7 @@ export default defineConfig([
     ignores: ['**/devcontainer.json', '**/.vscode/*.json', '**/package-lock.json'],
     plugins: { json, prettier },
     language: 'json/json',
-    extends: ['json/recommended'],
+    extends: [json.configs.recommended],
     rules: {
       'json/no-unsafe-values': 'off',
       'prettier/prettier': 'warn', // Use Prettier for formatting
@@ -182,7 +182,7 @@ export default defineConfig([
     files: ['**/*.jsonc', '**/devcontainer.json', '**/.vscode/*.json'],
     plugins: { json, prettier },
     language: 'json/jsonc',
-    extends: ['json/recommended'],
+    extends: [json.configs.recommended],
     rules: {
       'json/no-unsafe-values': 'off',
       'prettier/prettier': 'warn', // Use Prettier for formatting
@@ -192,7 +192,7 @@ export default defineConfig([
     name: 'Markdown Files',
     files: ['**/*.md'],
     plugins: { markdown, prettier },
-    extends: ['markdown/recommended'],
+    extends: [markdown.configs.recommended],
     rules: {
       'prettier/prettier': 'warn', // Use Prettier for formatting
     },
