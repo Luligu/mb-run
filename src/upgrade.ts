974701779b4bafbbf22327437d0e0546793da62b
Luligu <target.epithet-0h@icloud.com>
2026-08-01 11:59:53 +0100

    Add git merge rebase scripts

--- src/upgrade.ts
+++ src/upgrade.ts
@@ -648,7 +648,12 @@ export async function runPackageJsonUpgrade(
       'git:hardreset:main': automator?.git === true ? 'git fetch origin && git checkout main && git reset --hard origin/main' : undefined,
       'git:hardreset:dev': automator?.git === true ? 'git fetch origin && git checkout dev && git reset --hard origin/dev' : undefined,
       'git:hardreset:edge': automator?.git === true ? 'git fetch origin && git checkout edge && git reset --hard origin/edge' : undefined,
-      'git:rebase:dev': automator?.git === true ? 'git fetch origin && git checkout -b dev-backup && git checkout dev && git merge origin/main && git push origin dev' : undefined,
+      'git:merge:dev':
+        automator?.git === true ? 'git fetch origin && git checkout -b dev-backup-001 && git checkout dev && git merge origin/main && git push origin dev' : undefined,
+      'git:rebase:dev':
+        automator?.git === true
+          ? 'git fetch origin && git checkout -b dev-backup-001 && git checkout dev && git rebase origin/main && git push --force-with-lease origin dev'
+          : undefined,
       'reset': 'npm run deepClean && npm run softReset',
       'softReset': `npm install --no-fund --no-audit && npm prune --no-fund --no-audit${isPlugin ? ' && npm link --no-fund --no-audit matterbridge' : ''} && npm run build && npm run typecheck`,
       'checkDependencies': `npm install --no-fund --no-audit --no-save npm-check-updates && ncu && npm run softReset`,
